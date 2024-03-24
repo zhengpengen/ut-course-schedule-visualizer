130c45431e6d06382953cf75709da6bf7b16c202
Michael Zheng <zhengpengen@gmail.com>
2024-03-24 01:16:59 -0500

    initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ut-course-schedule-visualizer
+# this is the repo for the UT CSV for Spring 2024 cs 378 Intro to HCI taught by Dr. Amy Pavel
+# please run ```npm install``` every time you pull for sanity check
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './App.css';
+import React, {useState} from 'react';
+
+
+function App() {
+  return (
+    <div className='container'>
+      good morning bois
+    </div>
+  );
+}
+
+export default App;
