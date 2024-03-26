f3cdfbcbafe92591a9bdeb30355f74bbb22fd7ff
HaSandhu <sandhunhs@gmail.com>
2024-03-26 11:39:37 -0500

    For testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, {useState} from 'react';
 import ClassGroup from './components/ClassGroupComponents/ClassGroup';
+import AllClassesList from './components/AllClassesList/AllClassesList'
 
 function App() {
   return (
     <div className='container'>
       good morning bois
       <ClassGroup />
+      <AllClassesList />
     </div>
   );
 }
