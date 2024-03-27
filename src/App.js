caac117ffae67be702e8978f854c7a7908c8569a
Michael Zheng <zhengpengen@gmail.com>
2024-03-27 01:14:45 -0500

    now have basic routing between course page and schedule page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import React, {useState} from 'react';
+import Schedules from './Schedules';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className='container'>
-      good morning bois
-    </div>
+    <Router>
+      <div className='container'>
+        <Switch>
+          <Route exact path='/'>
+            <h1>this is the main page where you drag and drop classes</h1>
+            <Link to='/schedules'>jump to schedules page</Link>
+          </Route>
+          <Route path='/schedules'>
+            <Schedules />
+          </Route>
+        </Switch>
+        
+      </div>
+    </Router>
+    
   );
 }
 
