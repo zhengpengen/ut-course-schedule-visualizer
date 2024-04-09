578ae422e7a23ec7aa4966ffb971747f2b674ec0
alantientran <att0316698@gmail.com>
2024-04-09 03:48:44 -0500

    clean up and full permutation generation

--- src/components/scheduleGenerator/scheduleGenerator.js
+++ src/components/scheduleGenerator/scheduleGenerator.js
@@ -1,105 +1,3 @@
-// import ExampleData from "../../ExampleData";
-
-import { computeHeadingLevel } from "@testing-library/react";
-
-// function schedule_generator() {
-//   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-//   const times = [
-//     "8:00",
-//     "9:00",
-//     "10:00",
-//     "11:00",
-//     "12:00",
-//     "13:00",
-//     "14:00",
-//     "15:00",
-//     "16:00",
-//     "17:00",
-//     "18:00",
-//     "19:00",
-//     "20:00",
-//   ];
-
-//   const allSchedules = [];
-
-//   const GroupCards = [
-//     [ExampleData[0], ExampleData[1]],
-//     [ExampleData[2], ExampleData[3]],
-//     [ExampleData[4], ExampleData[5]],
-//   ];
-
-//   const GroupCounts = [1, 2, 1];
-
-//   const getCourseForCell = (day, time) => {
-//     for (let course of courses) {
-//       if (
-//         course.weekday.includes(day) &&
-//         time >= course.start_time &&
-//         time < course.end_time
-//       ) {
-//         return course;
-//       }
-//     }
-//     return null;
-//   };
-
-//   // Permutate to allow each group to add a class first
-//   for (let i = 0; i < GroupCards.length; i++) {
-//     // Move the first element of GroupCards to the end
-//     const firstElement = GroupCards.shift();
-//     GroupCards.push(firstElement);
-
-//     // Call addClassesFromGroups with the updated GroupCards configuration
-//     const selectedClasses = addClassesFromGroups(GroupCards, GroupCounts);
-//     allSchedules.push(selectedClasses);
-//   }
-//   // selectedClasses.map(section => section.time_and_locations[0].start_time)
-//   console.log(allSchedules);
-//   return (
-//     <div>
-//       {allSchedules.map((schedule, index) => (
-//         <div key={index}>
-//           <h3>Schedule {index + 1}</h3>
-//           <table>
-//             <thead>
-//               <tr>
-//                 <th>Time</th>
-//                 {days.map((day) => (
-//                   <th key={day}>{day}</th>
-//                 ))}
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {times.map((time) => (
-//                 <tr key={time}>
-//                   <td>{time}</td>
-//                   {days.map((day) => {
-//                     const course = getCourseForCell(day, time, schedule);
-//                     return (
-//                       <td
-//                         key={day + time}
-//                         style={{
-//                           backgroundColor: course ? course.color : "#fff",
-//                         }}
-//                       >
-//                         {course
-//                           ? `${course.course_number} ${course.course_name}`
-//                           : ""}
-//                       </td>
-//                     );
-//                   })}
-//                 </tr>
-//               ))}
-//             </tbody>
-//           </table>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-/* original schedule_generator */
-
 export function schedule_generator(groupCardsList, groupCountsDict) {
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
   const times = [
@@ -119,24 +17,15 @@ export function schedule_generator(groupCardsList, groupCountsDict) {
   ];
 
   const allSchedules = [];
-  // const groupCardsandCounts = [];
-
-  // // 2D array of groupCards and groupCounts to keep them tgt when permutating
-  // for (let i = 0; i < groupCardsList.length; i++) {
-  //   groupCardsandCounts.push([
-  //     groupCardsList[i],
-  //     groupCountsDict[groupCardsList[i].id],
-  //   ]);
-  // }
   const groupPermutations = permute(groupCardsList);
 
   for (let i = 0; i < groupPermutations.length; i++) {
-    const GroupCards = groupPermutations[i]
-    let overallResults= [];
-    addClassesFromGroups(overallResults, GroupCards, {...groupCountsDict});
+    const GroupCards = groupPermutations[i];
+    let overallResults = [];
+    addClassesFromGroups(overallResults, GroupCards, { ...groupCountsDict });
     // console.log('all sch', allSchedules)
     // console.log('or',overallResults)
-    for(let c = 0; c < overallResults.length; c++){
+    for (let c = 0; c < overallResults.length; c++) {
       // console.log('s', overallResults[c])
       let duplicateDetected = allSchedules.some((arr) =>
         hasSameSectionIDs(arr, overallResults[c])
@@ -146,15 +35,7 @@ export function schedule_generator(groupCardsList, groupCountsDict) {
         allSchedules.push(overallResults[c]);
       }
     }
-    // let duplicateDetected = allSchedules.some((arr) =>
-    //   hasSameSectionIDs(arr, overallResults)
-    // );
-    // if (!duplicateDetected) {
-    //   console.log('or', overallResults)
-    //   allSchedules.push(...overallResults);
-    // }
   }
-  // selectedClasses.map(section => section.time_and_locations[0].start_time)
   return allSchedules;
 }
 
@@ -182,105 +63,12 @@ function permute(arr) {
   return result;
 }
 
-/*
-function addClassesFromGroups(GroupCards, GroupCounts) {
-  const selectedClasses = [];
-  // const selectedCourses = new Set();
-
-  console.log("selected classes is ", selectedClasses);
-  // Iterate over each group card
-  for (let groupIndex = 0; groupIndex < GroupCards.length; groupIndex++) {
-    const groupCard = GroupCards[groupIndex];
-    let addedCount = 0;
-
-    // for each class of a groupCard
-    for (const classObj of groupCard.classes) {
-      // for each section of a class
-      let addedSectionFromThisClass = false;
-      for (const section of classObj.sections) {
-        if (
-          addedSectionFromThisClass ||
-          addedCount >= GroupCounts[groupIndex]
-        ) {
-          break;
-        }
-        if (section.checked === true) {
-          let overlap = false;
-          // check if this section overlaps with any previously selected section
-          selectedClasses.forEach((selectedSection) => {
-            if (checkOverlap(section, selectedSection)) {
-              overlap = true;
-            }
-          });
-          if (!overlap) {
-            selectedClasses.push(section);
-            // selectedCourses.add(section.id);
-            addedCount++;
-            addedSectionFromThisClass = true;
-            break;
-          }
-        }
-      }
-    }
-
-    console.log(`Added ${addedCount} classes from Group ${groupIndex + 1}`);
-  }
-  console.log("selected classes is ", selectedClasses);
-  return selectedClasses;
-}
-*/
-
-// function addClassesFromGroups(GroupCards, GroupCounts) {
-//   const selectedClasses = [];
-//   // const selectedCourses = new Set();
-
-//   console.log("selected classes is ", selectedClasses);
-//   // Iterate over each group card
-//   for (let groupIndex = 0; groupIndex < GroupCards.length; groupIndex++) {
-//     const groupCard = GroupCards[groupIndex];
-//     let addedCount = 0;
-
-//     // for each class of a groupCard
-//     for (const classObj of groupCard.classes) {
-//       // for each section of a class
-//       let addedSectionFromThisClass = false;
-//       for (const section of classObj.sections) {
-//         if (
-//           addedSectionFromThisClass ||
-//           addedCount >= GroupCounts[groupIndex]
-//         ) {
-//           break;
-//         }
-//         if (section.checked === true) {
-//           let overlap = false;
-//           // check if this section overlaps with any previously selected section
-//           selectedClasses.forEach((selectedSection) => {
-//             if (checkOverlap(section, selectedSection)) {
-//               overlap = true;
-//             }
-//           });
-//           if (!overlap) {
-//             selectedClasses.push(section);
-//             // selectedCourses.add(section.id);
-//             addedCount++;
-//             addedSectionFromThisClass = true;
-//             break;
-//           }
-//         }
-//       }
-//     }
-
-//     console.log(`Added ${addedCount} classes from Group ${groupIndex + 1}`);
-//   }
-//   console.log("selected classes is ", selectedClasses);
-//   return selectedClasses;
-// }
 function addClassesFromGroups(
   overallResults,
   GroupCards,
   GroupCounts,
   selectedClasses = [],
-  groupIndex = 0,
+  groupIndex = 0
 ) {
   if (groupIndex >= GroupCards.length) {
     // If we've reached the end of GroupCards, add the current selectedClasses to the result
@@ -291,8 +79,6 @@ function addClassesFromGroups(
 
   const groupCard = GroupCards[groupIndex];
   const classCount = groupCard.classes.length;
-  
-  // const result = [];
 
   // Recursively explore each class in the current group
   for (let classIndex = 0; classIndex < classCount; classIndex++) {
@@ -302,10 +88,16 @@ function addClassesFromGroups(
     // Recursively explore each section in the current class
     for (let sectionIndex = 0; sectionIndex < sectionCount; sectionIndex++) {
       const section = classObj.sections[sectionIndex];
-      if(!section.checked){ // ignore this section
+      if (!section.checked) {
+        // ignore this section
         continue;
       }
-      section.className = classObj.course_major + " " + classObj.course_number + " " + classObj.course_name; // CONCATENATE CLASS PROPERTIES TO FORM CLASS NAME
+      section.className =
+        classObj.course_major +
+        " " +
+        classObj.course_number +
+        " " +
+        classObj.course_name; // CONCATENATE CLASS PROPERTIES TO FORM CLASS NAME
       let overlap = false;
 
       // Check if the section overlaps with any previously selected section
@@ -359,7 +151,7 @@ function hasSameSectionIDs(arr1, arr2) {
 
 function checkOverlap(section1, section2) {
   // Check if there's any overlap in timings
-  if(section1.className === section2.className) return true; // from the same class
+  if (section1.className === section2.className) return true; // from the same class
   for (const timeLoc1 of section1.time_and_locations) {
     for (const timeLoc2 of section2.time_and_locations) {
       if (timeLoc1.weekday.some((day) => timeLoc2.weekday.includes(day))) {
@@ -377,67 +169,3 @@ function checkOverlap(section1, section2) {
 }
 
 export default schedule_generator;
-
-// The code below can print one schedule
-// function schedule_generator() {
-//   // Assume classesArr is structured as described:
-//   const classesArr = [
-//     [
-//       {
-//         className: "Math 101",
-//         sections: [
-//           {
-//             startTime: "9:00 AM",
-//             endTime: "10:00 AM",
-//             sectionName: "Section A",
-//             professor: "John Doe",
-//           },
-//           {
-//             startTime: "10:15 AM",
-//             endTime: "11:15 AM",
-//             sectionName: "Section B",
-//             professor: "Jane Smith",
-//           },
-//           // Add more sections as needed
-//         ],
-//       },
-//       // Add more classes in this group as needed
-//     ],
-//     // Add more groups as needed
-//   ];
-
-//   // Loop through classesArr to access the start and end times of each section
-//   classesArr.forEach((group, groupIndex) => {
-//     console.log(`Group ${groupIndex + 1}:`);
-//     group.forEach((classItem, classIndex) => {
-//       console.log(`  Class ${classIndex + 1}: ${classItem.className}`);
-//       classItem.sections.forEach((section, sectionIndex) => {
-//         console.log(`    Section ${sectionIndex + 1}:`);
-//         console.log(`      Start Time: ${section.startTime}`);
-//         console.log(`      End Time: ${section.endTime}`);
-//         console.log(`      Section Name: ${section.sectionName}`);
-//         console.log(`      Professor: ${section.professor}`);
-//       });
-//     });
-//   });
-
-//   return <div></div>;
-// }
-
-// export default schedule_generator;
-
-// This code can print each section info
-
-// const section = GroupCards[0][0]; // Accessing the first section of the first group
-
-// console.log("Section details:");
-// console.log(`Course Major: ${section.course_major}`);
-// console.log(`Course Number: ${section.course_number}`);
-// console.log(`Course Name: ${section.course_name}`);
-// console.log(`Section ID: ${section.id}`);
-// console.log(`Professor: ${section.professor}`);
-// console.log(`Weekday: ${section.time_and_locations[0].weekday}`);
-// console.log(`Start Time: ${section.time_and_locations[0].start_time}`);
-// console.log(`End Time: ${section.time_and_locations[0].end_time}`);
-// console.log(`Location: ${section.time_and_locations[0].location}`);
-// console.log(`Checked: ${section.checked}`);
--- src/pages/GeneratedSchedulesPage/GeneratedSchedulesPage.js
+++ src/pages/GeneratedSchedulesPage/GeneratedSchedulesPage.js
@@ -3,23 +3,28 @@ import Schedule from "../../components/SchedulePageComponents/Schedule";
 import "./GeneratedSchedulesPage.css";
 import BackButton from "../../components/BackButton/BackButton";
 
-const GeneratedSchedulesPage = ({allSchedules}) => {
-
+const GeneratedSchedulesPage = ({ allSchedules }) => {
   return (
     <div className="generated-schedules">
       <BackButton />
       <h1>Generated Schedules</h1>
       {allSchedules.map((schedule, index) => (
         <div className="schedule">
-          <h3>
-            {index+1}
-          </h3>
+          <h3>{index + 1}</h3>
           {schedule.map((classEntry) => (
             <div className="classEntry">
               <div className="class-name">{classEntry.className}</div>
-              <div>{classEntry.id} {classEntry.professor}</div>
+              <div>
+                {classEntry.id} {classEntry.professor}
+              </div>
               {classEntry.time_and_locations.map((time_and_locations) => (
-                <div>{time_and_locations.location} | {time_and_locations.weekday?.toString().replaceAll(',',' ')} | {time_and_locations.start_time} {time_and_locations.start_time}{time_and_locations.start_time=='' ? '' : '-'}{time_and_locations.end_time}</div>
+                <div>
+                  {time_and_locations.location} |{" "}
+                  {time_and_locations.weekday?.toString().replaceAll(",", " ")}{" "}
+                  | {time_and_locations.start_time}
+                  {time_and_locations.start_time == "" ? "" : "-"}
+                  {time_and_locations.end_time}
+                </div>
               ))}
             </div>
           ))}
