c83ffa7572079f4bfa3e7653d9678cf9b456b579
Michael Zheng <zhengpengen@gmail.com>
2024-04-21 23:43:29 -0500

    changed it to 3 items per carousel page

--- src/pages/GeneratedSchedulesPage/GeneratedSchedulesPage.js
+++ src/pages/GeneratedSchedulesPage/GeneratedSchedulesPage.js
@@ -134,8 +134,8 @@ const GeneratedSchedulesPage = ({ allSchedules }) => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 4,
-      slidesToSlide: 4,
+      items: 3,
+      slidesToSlide: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
