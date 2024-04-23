ec2b38f16b686fcb7ead7b8c5f6a4056f4198125
Michael Zheng <zhengpengen@gmail.com>
2024-04-22 23:51:42 -0500

    moved parser back to assuming professor name is upper case

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
 
     section_loop: while (true) {
       console.log(input[index]);
-      while (isNaN(parseInt(input[index]))) {
+      while (input[index] === "" || isNaN(parseInt(input[index]))) {
         //get to the beginning of the next section
         if (index >= input.length) {
           break section_loop;
@@ -132,17 +132,10 @@ function App() {
       // get to professors
       section["professor"] = [];
 
-      // console.log(index, input[index])
-
       while (
-        !input[index].includes(",") ||
+        input[index] !== input[index].toUpperCase() ||
         input[index] === ""
       ) {
-        // console.log(index, input[index])
-        if(!isNaN(parseInt(input[index]))){ // reached new section
-          new_course["sections"].push(section);
-          continue section_loop;
-        }
         index += 1;
         if (index >= input.length) {
           // last section and no professor
@@ -151,21 +144,14 @@ function App() {
         }
       }
 
-      // console.log(index, input[index])
-
-      // if (!input[index].includes(",") && !input[index+1].includes(",")) {
-      //   // no professor, go to next section
-      //   new_course["sections"].push(section);
-      //   continue section_loop;
-      // }
+      if (!input[index].includes(",") && !input[index+1].includes(",")) {
+        // no professor, go to next section
+        new_course["sections"].push(section);
+        continue section_loop;
+      }
 
       let professor_name = "";
-      while (isNaN(parseInt(input[index]))) {
-        if (index >= input.length) {
-          // last section and no professor
-          new_course["sections"].push(section);
-          break section_loop;
-        }
+      while (input[index] === input[index].toUpperCase()) {
         if (input[index].includes(",")) {
           // found a professor
           professor_name += input[index] + " " + input[index+1];
